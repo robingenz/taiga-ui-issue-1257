802324f6eba4e8100ab78c8c67cc2b09d1be8269
Robin Genz <mail@robingenz.dev>
2022-01-25 21:57:20 +0100

    Setup jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  preset: "jest-preset-angular",
+  testMatch: ["<rootDir>/src/**/*.spec.ts"],
+};
+
+module.exports = config;
